6636707fb7bb86af3cc68989d5748f8aab8db3c9
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-03-19 12:19:17 +0100

    [feature]: analyzer and providers update to find hieroglyphics, generate and plot dataframes

---
+++ src/PathsProvider.py
@@ -0,0 +1,21 @@
+import os.path
+class PathsProvider:
+    def __init__(self):
+        self.DATASET = "../data/GlyphDataset/Dataset"
+        self.AUTOMATED = self.DATASET + "/Automated"
+        self.MANUAL = self.DATASET + "/Manual"
+        self.PICTURES = self.DATASET + "/Pictures"
+        self.AUTOMATED_LOCATIONS = self.AUTOMATED + "/Locations"
+        self.MANUAL_LOCATIONS = self.MANUAL + "/Locations"
+
+    # Returns the path from a hieroglyphic image based on the picture, number and gardiner code.
+    def get_path_by_picture_number_gardiner(self, picture, number, gardiner, mode="Preprocessed"):
+        if mode == "Preprocessed" or mode == "Raw":
+            path = f"{self.MANUAL}/{mode}/{int(picture)}/{picture}{number}_{gardiner}.png"
+            if not os.path.exists(path):
+                path = None
+        else:
+            path = None
+
+        return path
+
