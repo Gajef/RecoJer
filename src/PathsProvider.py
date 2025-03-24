054498af0cdc949363a424355f3591d7da1533d4
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-03-24 17:36:55 +0100

    [refactor] fix typo path provider typo

--- src/PathsProvider.py
+++ src/PathsProvider.py
@@ -49,7 +49,7 @@ class PathsProvider:
         filtered_paths =  list(filter(lambda file_path: file_path.split(".")[1] == extension, files_paths))
         return filtered_paths
 
-    def get_files_path_by_extention_in_order(self, path, extension):
+    def get_files_path_by_extension_in_order(self, path, extension):
         files_paths = self.get_files_by_extension_in_order(path, extension)
         files_paths = list(map(lambda file_path: path + "/" + file_path, files_paths))
         return files_paths
