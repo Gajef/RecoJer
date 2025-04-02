61b1ec3efbc0a61a3a16984285ce5ad44009314e
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-04-02 10:23:29 +0200

    [feature]: add creation dictionary for ascii dataset

--- src/YOLOFilesGenerator.py
+++ src/YOLOFilesGenerator.py
@@ -1,13 +1,17 @@
+import gardiner2unicode
 import numpy as np
 from GlyphdatasetAnalyzer import GlyphdatasetAnalyzer
 from PathsProvider import PathsProvider
+from gardiner2unicode import GardinerToUnicodeMap, UnicodeGlyphGenerator
+
 class YOLOFilesGenerator:
-    def __init__(self, n_occurrences=50):
+    def __init__(self, source, n_occurrences=50):
+        self.source = source
         self.analyzer = GlyphdatasetAnalyzer()
         self.paths = PathsProvider()
         self.complete_dataframe = self.analyzer.generate_dataframe(self.paths.MANUAL_LOCATIONS)
         self.gardiner_ids, self.selected_dataframe = self.__select_classes_from_df_with_n_occurrences(n_occurrences)
-        self.dictionary = self.__create_dictionary()
+        self.glyph_class_dictionary = self.__create_dictionary()
 
     # Translates txt from original form to YOLO form
     def translate_txt(self, original_file_path, new_file_path):
@@ -20,7 +24,7 @@ class YOLOFilesGenerator:
                     image_name, x_top, y_top, x_bottom, y_bottom, _ = line.split(",")
                 gardiner_id = image_name.split(".")[0].split("_")[1]
                 if gardiner_id in self.gardiner_ids:
-                    class_by_id = self.dictionary[gardiner_id]
+                    class_by_id = self.glyph_class_dictionary[gardiner_id]
 
                     x_center = ((int(x_top) + int(x_bottom)) / 2) / 1150
                     y_center = ((int(y_top) + int(y_bottom)) / 2) / 1600
@@ -39,14 +43,20 @@ class YOLOFilesGenerator:
                   f"train: images/train\n",
                   f"val: images/val\n",
                   "\n",
-                  # f"nc: {len(self.gardiner_ids)}\n",
                   f"names:\n"
                   ]
 
-            for key, value in self.dictionary.items():
+            for key, value in self.glyph_class_dictionary.items():
                 lines.append(f"    {value}: {key}\n")
 
             data_yaml.writelines(lines)
+
+    def generate_txt(self, annotations, filename):
+        with open(self.paths.ASCII_AUGMENTATION + "/labels/" + filename, "w") as text_file:
+            for annotation in annotations:
+                text_file.write(f"{annotation}\n")
+
+        text_file.close()
 
     def __create_dictionary(self):
         classes_number = np.arange(0, len(self.gardiner_ids))
@@ -58,6 +68,10 @@ class YOLOFilesGenerator:
 
 
     def __select_classes_from_df_with_n_occurrences(self, n_occurrences):
-        dataframe = self.analyzer.find_hieroglyphics_with_at_least_n_occurrences(self.complete_dataframe, 50)
-
-        return dataframe
+        if self.source == "glyphdataset":
+            result = self.analyzer.find_hieroglyphics_with_at_least_n_occurrences(self.complete_dataframe, n_occurrences)
+        else:
+            ids = GardinerToUnicodeMap().gardiner2unicode.keys()
+            dataframe = None
+            result = [ids, dataframe]
+        return result
