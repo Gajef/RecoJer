177aa0c8d9e8ceae493f2172a9c82b4300ba5df1
Jesus Garcia <jesus.garcia1650@gmail.com>
2025-04-03 12:30:39 +0200

    [refactor]: add class to be usable outside the file

--- src/PTBookImporter.py
+++ src/PTBookImporter.py
@@ -6,32 +6,38 @@ from pdf2image import convert_from_path
 from os import listdir
 from os.path import isfile, join
 from PathsProvider import PathsProvider
+
 # path='/home/vruiz/RecoJer/data/LibroMuertos/'
 paths = PathsProvider()
 path = paths.PYRAMIDTEXTS_BOOK
-gray = True
 
-if gray:
-    dst_path = "/imagenes_jpg_gris"
-else:
-    dst_path = "/imagenes_jpg"
-onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-print(onlyfiles)
+class PTBookImporter:
+    def __init__(self, book_path=path, gray=False):
+        self.book_path = book_path
+        self.gray = gray
+        if gray:
+            self.dst_path = "/imagenes_jpg_gris"
+        else:
+            self.dst_path = "/imagenes_jpg"
 
-# Convierto pdf en jpg
-for d in range(len(onlyfiles)):
+    def generate_images(self):
+        onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+        print(onlyfiles)
 
-    number_doc = int(onlyfiles[d].partition("PT ")[2][0])
-    if number_doc > 1:  # Importo imágenes de todos los documentos menos del primero
-        file_path = path + onlyfiles[d]
-        # Store Pdf with convert_from_path function
-        images = convert_from_path(file_path)
+        # Convierto pdf en jpg
+        for d in range(len(onlyfiles)):
 
-        for i in range(len(images)):
-            if i > 1:
-                if gray:
-                    image = images[i].convert('L')
-                else:
-                    image = images[i]
-                # Save pages as images in the pdf
-                image.save(path + dst_path + '/doc_' + str(number_doc) + '_page_' + str(i) + '.jpg', 'JPEG')
+            number_doc = int(onlyfiles[d].partition("PT ")[2][0])
+            if number_doc > 1:  # Importo imágenes de todos los documentos menos del primero
+                file_path = path + onlyfiles[d]
+                # Store Pdf with convert_from_path function
+                images = convert_from_path(file_path)
+
+                for i in range(len(images)):
+                    if i > 1:
+                        if self.gray:
+                            image = images[i].convert('L')
+                        else:
+                            image = images[i]
+                        # Save pages as images in the pdf
+                        image.save(self.book_path + self.dst_path + '/doc_' + str(number_doc) + '_page_' + str(i) + '.jpg', 'JPEG')
